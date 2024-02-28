1cd4901dbd3743d499a85b38240a573af31c3cb5
Diamondra <99741422+dranadison@users.noreply.github.com>
2024-02-28 06:56:43 +0300

    navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { AuthentificationService } from '../authentification.service';
+import { SharedService } from '../services/shared.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
+  template: `
+  <div *ngIf="divVisible$ | async">
+    <!-- Your content goes here -->
+  </div>
+  `
 })
 export class NavbarComponent {
-  constructor(private authService: AuthentificationService){}
+  constructor(private authService: AuthentificationService,private sharedService: SharedService){}
+
+  divVisible$ = this.sharedService.divVisibility$;
 
   isManager(){
     return this.authService.isManager()
@@ -20,4 +28,8 @@ export class NavbarComponent {
   isClient(){
     return this.authService.isClient()
   }
+  toggleDiv() {
+    this.sharedService.toggleDivVisibility();
+  }
 }
+
