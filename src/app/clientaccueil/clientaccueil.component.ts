b3ba5432855629c55c0e939f5ed5cc17736e2df7
Diamondra <dranadison@gmail.com>
2024-02-29 23:34:29 +0300

    Offre special

--- src/app/clientaccueil/clientaccueil.component.ts
+++ src/app/clientaccueil/clientaccueil.component.ts
@@ -3,6 +3,9 @@ import { AuthentificationService } from '../authentification.service';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { RDVDataUnit, RDVUnit, RDVDataTotal } from '../Types/RDV';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { RDV } from '../Types/RDV';
+import { DatePipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { Offres } from '../Types/Offre';
 
@@ -20,11 +23,14 @@ export class ClientaccueilComponent {
   selectedOffreId: any;
   rdvDetails:  any;
   offreDetails:  any;
+  errorMessage = ''
 
-  constructor(private authService: AuthentificationService, private http: HttpClient){}
+  constructor(private authService: AuthentificationService, private http: HttpClient,private datePipe: DatePipe,){}
   rdv :any
   totalRdv: any;
   offres : Offres = []
+  selectedDate: Date|null = null;
+  selectedTime: string = '';
 
   fetchRDV() : Observable<any> {
     const headers = new HttpHeaders({
@@ -102,7 +108,46 @@ export class ClientaccueilComponent {
     return this.authService.isManager()
   }
 
-  
+  onDateChange(event: MatDatepickerInputEvent<Date> ){
+    this.selectedDate = event.value;
+  }
+
+  sendData(credentials: RDV) : Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.authService.getToken()}`
+    });
+    return this.http.post('https://m1p10mean-fanjava-diamondra-back.vercel.app/rdv/add',credentials,{headers: headers});
+  }
+
+  addRDV(){
+    this.errorMessage ='';
+    if(this.selectedDate === null || this.selectedTime === ''){
+      this.errorMessage="Veuillez définir la date et l'heure";
+      return;
+    }
+    const paramService: any[] = [];
+    this.offreDetails.forEach(((service: { idService: any; prix:any; }) =>{
+      const s = {idService: service.idService, idEmploye: null, prix: service.prix };
+      paramService.push(s);
+    }))
+    console.log(paramService);
+    
+    const times = this.selectedTime.split(':');
+    this.selectedDate.setHours(parseInt(times[0]));
+    this.selectedDate.setMinutes(parseInt(times[1]));
+    const dateHeure = this.datePipe.transform(this.selectedDate,'yyyy-MM-dd HH:mm:ss')
+    const rdv: RDV ={
+      dateHeure: dateHeure,
+      service: paramService
+    };
+    this.sendData(rdv).subscribe(succes => {
+      this.errorMessage = '';
+      this.selectedDate = null;
+      this.selectedTime = '';
+    }, error => {
+        this.errorMessage=error.error.message;
+    })
+  }
   
 
 
